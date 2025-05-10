96cc99029c591322873d674bd2aa32bcfbf86a1b
AiRok386 <rahulthakur432001@gmail.com>
2025-05-11 01:29:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,3 +17,4 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.use('/api/auth', require('./routes/auth.routes'));
